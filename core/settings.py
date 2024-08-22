d1145b51a7c5b55016673f962ca382e32e36cfd7
vinhta916 <vta@gridsec.com>
2024-08-22 12:49:03 -0700

    Update where environment variable is

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+environ.Env.read_env('django.env') # reading .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
