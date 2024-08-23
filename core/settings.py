c2480444aec66c5205073c74b4792dd85c025174
vinhta916 <vta@gridsec.com>
2024-08-23 13:23:11 -0700

    Update static url

--- core/settings.py
+++ core/settings.py
@@ -125,11 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
+
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
 CELERY_TIMEZONE = "US/Pacific"
 
 environment = os.environ.get("DB")
