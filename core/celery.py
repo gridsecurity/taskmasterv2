3704376d5af7fdcb88eca78ca06c0b5b5d724a42
vinhta916 <vta@gridsec.com>
2024-08-26 07:27:17 -0700

    Add celery beat task

--- core/celery.py
+++ core/celery.py
@@ -13,14 +13,3 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # "namespace" refers to a container or scope that holds a set of identifiers (such as variables, functions, classes, or objects)
 
 app.autodiscover_tasks()
-
-print("adding periodic tasks")
-app.conf.beat_schedule = {
-    # Executes every Monday morning at 7:30 a.m.
-    'add-every-monday-morning': {
-        'task': 'tasks.add',
-        'schedule': crontab(minute="*/1"),
-        'args': (16, 16),
-    },
-}
-print("added periodic tasks")
